2c0e15a064d1c37b2f710638f46ede9026391445
dk1Dhruv <dk3482963@gmail.com>
2026-08-13 08:05:47 +0530

    Time: 11 ms (12.68%), Space: 8.6 MB (51.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,16 +2,8 @@ class Solution {
 public:
     int mySqrt(int x) {
         long long ans=0;
-        if(x>1 || x==1){
-              for(long long i=1;i*i<=x;i++){
-              
-                    ans=i;}
-                   
-              return ans;
-           
-        }
-        else{
-        return 0;
-        }
+        for(long long i=0;i*i<=x;i++){
+              ans=i;}
+    return ans;
     }
 };
