e2612be28fbcf20af608993f1c2749bd9fad5804
dk1Dhruv <dk3482963@gmail.com>
2026-09-07 22:16:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.43%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int n) {
-            if (n == 1) {
-            return true;
-        }
-         
-    for(long long i=2;i*i<=n;i++){
+     for(long long i=1;i*i<=n;i++){
         if(i*i==n){
             return true;
         }
